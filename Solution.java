9f2920585da8b4b3207dad79ff41ff5ebd0e7de3
kasib <kasibzxc@gmail.com>
2023-10-31 14:16:38 -0400

    solved mostFrequent with ordered hashmap

--- Solution.java
+++ Solution.java
@@ -1,12 +1,43 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 class Solution {
     public static char mostFrequentChar(String s) {
+      HashMap<String, Integer> map = new LinkedHashMap<>();
+
+      // split string to single length strings.
+      String[] letters = s.split("");
+      for (String letter : letters) { // iterate through string, check if letter in map
+        if (map.containsKey(letter)) {
+          int count = map.get(letter);
+          count++;
+          map.put(letter, count);
+        } else {
+          map.put(letter, 1);
+        }
+      }
+
+      int[] max = {0};
+      char[] res = new char[1];
+      map.forEach((key, value) -> {
+        if (map.get(key) > max[0]) {
+          max[0] = map.get(key);
+          res[0] = key.charAt(0);
+        }
+
+        
+      });
       
-      return;
+      return res[0];
     }
   
     public static void main(String[] args) {
       // Try out your test cases:
       System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+      System.out.println(Solution.mostFrequentChar("david")); // -> 'd'
+      System.out.println(Solution.mostFrequentChar("mississippi")); // -> 'i'
+      System.out.println(Solution.mostFrequentChar("eleventennine")); // -> 'e'
+      System.out.println(Solution.mostFrequentChar("eeevvv"));
     }
 
   }
